3e63d9b0856c7b1b1adf346f0f20f818526f9e7d
Bryan Harter <41062454+bryan-harter@users.noreply.github.com>
2024-11-21 17:26:43 +0000

    Bug fix

--- scripts/ingest_anc_files/bundle_trk_files.sh
+++ scripts/ingest_anc_files/bundle_trk_files.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-python3 "bundle_trk_files.py" "-t" "full" "-o" "/maven/data/sdc/trk/"
+python3 "$SCRIPT_DIR/bundle_trk_files.py" "-t" "full" "-o" "/maven/data/sdc/trk/"
