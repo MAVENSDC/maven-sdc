0da0c61800664a0ace06a0be131d82aee9fbdc75
Bryan Harter <41062454+bryan-harter@users.noreply.github.com>
2024-11-21 17:24:30 +0000

    Fixing TRK bundler

--- scripts/ingest_anc_files/bundle_trk_files.sh
+++ scripts/ingest_anc_files/bundle_trk_files.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-bundle_trk_files.py -t full -o /maven/data/sdc/trk/
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+python3 "bundle_trk_files.py" "-t" "full" "-o" "/maven/data/sdc/trk/"
