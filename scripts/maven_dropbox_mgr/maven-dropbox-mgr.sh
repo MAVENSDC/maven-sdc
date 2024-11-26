49101d1cda2f9f9bd92de4c4d2ac55079a701e2b
Bryan Harter <41062454+bryan-harter@users.noreply.github.com>
2024-11-26 21:44:33 +0000

    Allowing the dropbox manager to move in if there is an error in one of the dropboxes

--- scripts/maven_dropbox_mgr/maven-dropbox-mgr.sh
+++ scripts/maven_dropbox_mgr/maven-dropbox-mgr.sh
@@ -3,13 +3,13 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 # Run the dropbox manager giving it a list of itf dropbox directories.
-python3 "$SCRIPT_DIR/maven-dropbox-mgr.py" "/maven/itfhome/acc/dropbox" \
-                     "/maven/itfhome/iuvs/dropbox" \
-                     "/maven/itfhome/lpw/dropbox" \
-                     "/maven/itfhome/radio/dropbox" \
-                     "/maven/itfhome/euv/dropbox" \
-                     "/maven/itfhome/mag/dropbox" \
-                     "/maven/itfhome/ngims/dropbox" \
-                     "/maven/itfhome/pf/dropbox" \
-                     "/maven/itfhome/mavenjpl/dropbox" \
-                     "/maven/itfhome/sweal3/dropbox"
+python3 "$SCRIPT_DIR/maven-dropbox-mgr.py" "/maven/itfhome/acc/dropbox"
+python3 "$SCRIPT_DIR/maven-dropbox-mgr.py" "/maven/itfhome/iuvs/dropbox"
+python3 "$SCRIPT_DIR/maven-dropbox-mgr.py" "/maven/itfhome/lpw/dropbox"
+python3 "$SCRIPT_DIR/maven-dropbox-mgr.py" "/maven/itfhome/radio/dropbox"
+python3 "$SCRIPT_DIR/maven-dropbox-mgr.py" "/maven/itfhome/euv/dropbox"
+python3 "$SCRIPT_DIR/maven-dropbox-mgr.py" "/maven/itfhome/mag/dropbox" 
+python3 "$SCRIPT_DIR/maven-dropbox-mgr.py" "/maven/itfhome/ngims/dropbox" 
+python3 "$SCRIPT_DIR/maven-dropbox-mgr.py" "/maven/itfhome/pf/dropbox" 
+python3 "$SCRIPT_DIR/maven-dropbox-mgr.py" "/maven/itfhome/mavenjpl/dropbox" 
+python3 "$SCRIPT_DIR/maven-dropbox-mgr.py" "/maven/itfhome/sweal3/dropbox"
