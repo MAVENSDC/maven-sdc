841ffb0c427ed8e5246498eaa98eb96e1e6fc861
Bryan Harter <41062454+bryan-harter@users.noreply.github.com>
2025-11-06 22:58:21 +0000

    Adding a period

--- maven_dropbox_mgr/__init__.py
+++ maven_dropbox_mgr/__init__.py
@@ -4,5 +4,5 @@ from datetime import tzinfo
 from maven_utilities import time_utilities
 
 # Time used to determine when the maven_dropbox_mgr package was initialized.
-# This is used to create dated directories for a set of dropbox items
+# This is used to create dated directories for a set of dropbox items.
 init_time = time_utilities.utc_now()
